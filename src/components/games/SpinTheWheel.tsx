758f4c744579c3ff58aed9da65a22287b0dbec22
Osama Mahmoud <ousama.mah113@gmail.com>
2024-06-10 10:57:52 +0300

    Create Spin Wheel.

--- src/components/games/SpinTheWheel.tsx
+++ src/components/games/SpinTheWheel.tsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import useSpinGame from "../../hooks/useSpinGame";
+import { FieldValues, useForm, UseFormRegister } from "react-hook-form";
+interface IFormInput {
+  [key: string]: string | number;
+}
 
 const SpinTheWheel: React.FC = () => {
   const { spinGameInform } = useSpinGame();
+  const { register, handleSubmit } = useForm<IFormInput>({
+    defaultValues: {
+      spinInput_0: spinGameInform.digit_one,
+      spinInput_1: spinGameInform.digit_two,
+      spinInput_2: spinGameInform.digit_three,
+      spinInput_3: spinGameInform.digit_four,
+      spinInput_4: spinGameInform.digit_five,
+      spinInput_5: spinGameInform.digit_six,
+      spinInput_6: spinGameInform.digit_seven,
+      spinInput_7: spinGameInform.digit_eight,
+      spinInput_8: spinGameInform.digit_nine,
+    },
+  });
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // Logic to handle input change, if needed
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
   };
 
   return (
-    <div className="container mx-auto px-6 flex flex-col">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="container mx-auto px-6 flex flex-col"
+    >
       <div className="flex justify-between items-center my-10">
         <h1 className="text-4xl text-[#577656]">Spin the Wheel</h1>
         <p className="text-4xl text-[#6A6868]">59 : 00</p>
       </div>
       <div className="flex flex-col justify-center items-center w-fit self-center">
-        <div className="">
+        <div>
           <img src="/assets/games/wheel.svg" alt="Spin the Wheel" />
         </div>
         {[
@@ -33,50 +53,70 @@ const SpinTheWheel: React.FC = () => {
             key={index}
             title={spin.title}
             digit={spin.digit}
-            onChange={handleInputChange}
+            register={register}
+            inputName={`spinInput_${index}`}
           />
         ))}
       </div>
       <button className="text-2xl bg-[#577656] text-white rounded-md px-20 py-4 self-center mt-8 hover:bg-[#A0D8B3] transition-all duration-150">
         Apply
       </button>
-    </div>
+    </form>
   );
 };
 
 export default SpinTheWheel;
 
-
 interface SpinInputProps {
   title: string;
   digit: number | string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  register: UseFormRegister<IFormInput>;
+  inputName: string;
 }
 
-const SpinInput: React.FC<SpinInputProps> = ({ title, digit, onChange }) => {
+const SpinInput: React.FC<SpinInputProps> = ({
+  title,
+  digit,
+  register,
+  inputName,
+}) => {
   const isSpinAgain = typeof digit === "string";
+  const [isChecked, setChecked] = useState<boolean>(typeof digit === "string");
+  const handleCheckedBox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(e.currentTarget.checked);
+  };
 
   return (
     <div className="mt-10">
       <h2 className="text-4xl font-semibold">{title}</h2>
       <div className="flex flex-col my-5 w-full">
-        <label className="label text-[#475467] text-xl">Discount percentage</label>
+        <label className="label text-[#475467] text-xl">
+          Discount percentage
+        </label>
         <div className="flex gap-4 justify-center items-center">
-        {isSpinAgain ? <input
-            type="number"
-            className="input input-bordered xl:w-[500px]"
-            onChange={onChange}
-            disabled
-          /> :
-          <input
-            defaultValue={digit}
-            type="number"
-            className="input input-bordered xl:w-[500px]"
-            onChange={onChange}
-          /> 
-          }
+          {isChecked ? (
+            <input
+              {...register(inputName)}
+              type="number"
+              className="input input-bordered xl:w-[500px]"
+              disabled
+            />
+          ) : (
+            <input
+              {...register(inputName)}
+              defaultValue={digit}
+              type="number"
+              className="input input-bordered xl:w-[500px]"
+            />
+          )}
           <div>
-            <input type="checkbox" checked={isSpinAgain}  /> <span>Spin again</span>
+            <input
+              className="mr-2"
+              type="checkbox"
+              checked={isChecked}
+              onChange={handleCheckedBox}
+            />
+            <span>Spin again</span>
           </div>
         </div>
       </div>
